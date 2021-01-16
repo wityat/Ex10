1b96aaf17e65e79cf409550ff804b6aba2f3ffa2
anton <ashtanyuk@gmail.com>
2021-01-16 21:07:00 +0300

    Edited 4

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,5 +7,5 @@ TEST(PostfixTest, test1) {
   std::string inf = "2 + 2";
   std::string postf = infix2postfix(inf);
   std::string expected = "2 2 +";
-  EXPECT_EQ(expected, post);
+  EXPECT_EQ(expected, postf);
 }
